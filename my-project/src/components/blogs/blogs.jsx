887e10167542412855eff8974f2265309287b0b2
Najifa Montaha Artika <najifamontahaartika@gmail.com>
2024-10-21 22:55:30 +0600

    second commit

--- my-project/src/components/blogs/blogs.jsx
+++ my-project/src/components/blogs/blogs.jsx
@@ -1,9 +1,8 @@
 import { useEffect } from "react";
 import { useState } from "react";
-
+import Blog from "../blog/blog";
 const Blogs = () => {
     const [blogs, setblogs] = useState([]);
-
     useEffect(() => {
         fetch('blogs.json')
         .then(res => res.json())
@@ -12,8 +11,10 @@ const Blogs = () => {
     return (
         <div className="md:w-2/3">
             <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+            {
+                blogs.map(blog => <Blog key={blog.id} blog={blog}></Blog>)
+            }
         </div>
     );
 };
-
 export default Blogs;
